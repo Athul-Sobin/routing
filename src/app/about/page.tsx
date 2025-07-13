381085ddb974a9a43b6097d41a725a2a6f5e071e
Athul-Sobin <athulsobin2005@gmail.com>
2025-07-13 22:29:20 +0530

    Initial commit

---
+++ src/app/about/page.tsx
@@ -0,0 +1,10 @@
+import Link from 'next/link';
+
+export default function About() {
+  return (
+    <div>
+      <h1>About Page</h1>
+      <p>Welcome to About Page</p>
+    </div>
+  );
+}
---
+++ src/app/contact/page.tsx
@@ -0,0 +1,10 @@
+import Link from 'next/link';
+
+export default function Contact() {
+  return (
+    <div>
+      <h1>Contact Page</h1>
+      <p>Welcome to Contact Page</p>
+    </div>
+  );
+}
